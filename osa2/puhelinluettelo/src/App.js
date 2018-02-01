d0a38249b9c1ec9a449ba211d9973006f43cc30f
BikkuMyy <maripartan@gmail.com>
2018-02-01 23:19:33 +0200

    puhelinluettelo osa 6'

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Person from './components/Person'
+import axios from 'axios'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            persons: [
-                { name: 'Arto Hellas', number: '040-1234567' },
-                { name: 'Martti Tienari', number: '040-123456' },
-                { name: 'Arto Järvinen', number: '040-123456' },
-                { name: 'Lea Kutvonen', number: '040-123456' }
-            ],
+            persons: [],
             newName: '',
             newNumber: '',
             filter: ''
         }
+    }
+
+    componentWillMount() {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                this.setState({ persons: response.data})
+            })
     }
 
     handleNameChange = (event) => {
